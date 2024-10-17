8f2189f179b3507797d917ea98fe67824ae4f41c
ruslanbayramov1 <ruslanyb-ab108@code.edu.az>
2024-10-17 22:03:02 +0400

    access modifiers task 2

---
+++ 09_Access_Modifiers_Employee_Task/Program.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+
+namespace _09_Access_Modifiers_Employee_Task
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ruslan = new Employee("Ruslan", true, 4500);
+            ruslan.PrintEmployeeInfo();
+
+            Assistant assistant = new Assistant();
+            assistant.GetFeedBack(ruslan);
+
+            ruslan.PrintEmployeeInfo();
+        }
+    }
+}
