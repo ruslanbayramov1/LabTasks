9482e1b03161d912e09a39dcbe6608434c521ebe
ruslanbayramov1 <ruslanyb-ab108@code.edu.az>
2024-10-19 16:14:38 +0400

    data relationships between 2 different table

---
+++ 10_Data_Relations_Project/Models/Project.cs
@@ -0,0 +1,277 @@
+﻿namespace _10_Data_Relations_Project.Models
+{
+    internal class Project
+    {
+        #region Encapsulation
+        private int _id;
+        private string _name;
+        private Employee[] _employees = { }; //prevent from error
+        private DateTime _startTime;
+        private DateTime _deadline;
+        private string _status;
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public Employee[] Employees
+        {
+            get { return _employees; }
+            set
+            {
+                _employees = value;
+            }
+        }
+
+        private DateTime StartTime
+        {
+            get { return _startTime; }
+            set
+            {
+                _startTime = value;
+            }
+        }
+
+        private DateTime Deadline
+        {
+            get { return _deadline; }
+            set
+            {
+                _deadline = value;
+            }
+        }
+
+        private string Status 
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+            }
+        }
+        #endregion
+
+        public Project(int projId, string projName)
+        {
+            Id = projId;
+            Name = projName;
+            StartTime = DateTime.Now;
+            Deadline = new DateTime(StartTime.Year, StartTime.Month + 1, StartTime.Day);
+            Status = "Working...";
+        }
+
+        public void GetInfo()
+        {
+            DateTime current = DateTime.Now;
+            Console.WriteLine($"Id: {Id}\nProject: {Name}\nNumber of employees: {Employees.Length}\nStatus: {Status}");
+            Console.WriteLine($"StartDate: {StartTime}\nDeadline: {Deadline}\nDays remains: {(Deadline - current).Days}");
+        }
+
+        public void GetInfoEmployees()
+        {
+            if (Employees.Length >= 1)
+            {
+                Console.WriteLine($"{Name}'s all employees:");
+                int count = 0;
+                foreach (Employee employee in Employees)
+                {
+                    count++;
+                    Console.WriteLine($"{count}.\nId: {employee.Id}\nName: {employee.FullName}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The project {Name} have no employee");
+            }
+        }
+
+        public void AssignEmployee(Employee givenEmployee)
+        {
+            Employee[] clonedEmployees = (Employee[])Employees.Clone();
+            Array.Resize(ref clonedEmployees, clonedEmployees.Length + 1);
+            clonedEmployees[clonedEmployees.Length - 1] = givenEmployee;
+            Employees = clonedEmployees;
+
+            // adding project to given employee
+            AssignProjectToEmployee(givenEmployee);
+        }
+
+        private void AssignProjectToEmployee(Employee givenEmployee)
+        {
+            Project[] clonedProjects = (Project[])givenEmployee.Projects.Clone();
+            Array.Resize(ref clonedProjects, clonedProjects.Length + 1);
+            clonedProjects[clonedProjects.Length - 1] = this;
+            givenEmployee.Projects = clonedProjects;
+        }
+
+        public void RemoveEmployee(int employeeId)
+        {
+            int length = 0;
+            for (int i = 0; i < Employees.Length; i++)
+            {
+                if (Employees[i].Id != employeeId) length++;
+            }
+
+            if (length == Employees.Length)
+            {
+                return;
+            }
+
+            Employee[] tempEmployees = new Employee[length];
+            int index = 0;
+            int remIndex = 0;
+            for (int i = 0; i < Employees.Length; i++)
+            {
+                if (Employees[i].Id != employeeId)
+                {
+                    tempEmployees[index] = Employees[i];
+                    index++;
+                }
+                else if (Employees[i].Id == employeeId) remIndex = i;
+            }
+
+            Employees[remIndex].RemoveProject(Id); // remindex is removed employee and calling its leave method for remove project from employee
+
+            Employees = tempEmployees; // last action to re-assign removed element array
+        }
+
+        public void Finish()
+        {
+            DateTime curretTime = DateTime.Now;
+            if (curretTime > Deadline)
+            {
+                Status = "Fail";
+                Console.WriteLine($"Oops! You reached the deadline, unsuccessful project! Status {Status} added");
+            }
+            else if (curretTime <= Deadline && curretTime >= StartTime)
+            { 
+                Status = "Successful";
+                Console.WriteLine($"The project finished successfully. Status {Status} added.");
+            }
+        }
+    }
+
+    internal class Employee
+    {
+        #region Encapsulation
+        private int _id;
+        private string _fullName;
+        private Project[] _projects = { };
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                _fullName = value;
+            }
+        }
+
+        public Project[] Projects
+        {
+            get { return _projects; }
+            set
+            {
+                _projects = value;
+            }
+        }
+        #endregion
+
+        public Employee(int id, string fullName)
+        {
+            Id = id;
+            FullName = fullName;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Id: {Id}\nName: {FullName}\nNumber of projects: {Projects.Length}");
+        }
+
+        public void GetInfoProjects()
+        {
+            if (Projects.Length >= 1)
+            {
+                Console.WriteLine($"{FullName}'s all projects:");
+                int count = 0;
+                foreach (Project project in Projects)
+                {
+                    count++;
+                    Console.WriteLine($"{count}.\nId: {project.Id}\nName: {project.Name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The employee {FullName} have no projects");
+            }
+        }
+
+        public Project RemoveProject(int projectId) // removes project from employee
+        {
+            int length = 0;
+            for (int i = 0; i < Projects.Length; i++)
+            {
+                if (Projects[i].Id != projectId) length++;
+            }
+
+            if (length == Projects.Length)
+            {
+                return null;
+            }
+
+            Project[] tempProjects = new Project[length];
+            int index = 0;
+            int remIndex = 0;
+            for (int i = 0; i < Projects.Length; i++)
+            {
+                if (Projects[i].Id != projectId)
+                {
+                    tempProjects[index] = Projects[i];
+                    index++;
+                }
+                else if (Projects[i].Id == projectId) remIndex = i;
+            }
+
+            Project removedPrj = Projects[remIndex];
+
+            Projects = tempProjects; // last action to re-assign removed element array
+
+            return removedPrj;
+        }
+
+        public void Leave(int projectId)
+        {
+            Project removedPrj = RemoveProject(projectId);
+
+            if (removedPrj == null)
+            {
+                return;
+            }
+
+            removedPrj.RemoveEmployee(Id);
+        }
+    }
+}
---
+++ 10_Data_Relations_Project/Program.cs
@@ -0,0 +1,343 @@
+﻿using _10_Data_Relations_Project.Models;
+
+namespace _10_Data_Relations_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Data Relation model!\nPress enter to start!");
+            Console.ReadLine();
+
+            Console.WriteLine("0 - Create project\n1 - Create employee\n2 - All projects\n3 - All employees");
+            Console.WriteLine("4 - Project detailed info\n5 - Employee detailed info");
+            Console.WriteLine("6 - Project employees info\n7 - Employee project info");
+            Console.WriteLine("8 - Assign employee to project\n9 - Remove employee from project");
+            Console.WriteLine("10 - Leave project\n11 - Finish the project\n12 - Exit the dashboard\n");
+
+            Project[] AllProjects = new Project[0];
+            Project[] CopyOfAllProjects = (Project[])AllProjects.Clone();
+            Employee[] AllEmployees = new Employee[0];
+            Employee[] CopyOfAllEmployees = (Employee[])AllEmployees.Clone();
+
+            int projId;
+            string projName;
+            int empId;
+            string empName;
+
+            int prjIndex = 0;
+            int empIndex = 0;
+
+            bool isPlaying = true;
+            int code;
+            while (isPlaying)
+            {
+                Console.Write("\nTry to permorm an act: ");
+                code = Convert.ToInt32(Console.ReadLine());
+
+                switch (code)
+                {
+                    case 0:
+                        Console.Write("Enter project name: ");
+                        projName = Console.ReadLine().Trim();
+                        Console.Write("Enter project id: ");
+                        projId = Convert.ToInt32(Console.ReadLine());
+
+                        CreateProject(ref AllProjects, ref CopyOfAllProjects, projId, projName);
+                        Console.WriteLine($"Project {AllProjects[AllProjects.Length - 1].Name} is created!");
+                        break;
+                    case 1:
+                        Console.Write("Enter employee name: ");
+                        empName = Console.ReadLine().Trim();
+                        Console.Write("Enter employee id: ");
+                        empId = Convert.ToInt32(Console.ReadLine());
+
+                        CreateEmployee(ref AllEmployees, ref CopyOfAllEmployees, empId, empName);
+                        Console.WriteLine($"Employee {AllEmployees[AllEmployees.Length - 1].FullName} is created!");
+                        break;
+                    case 2:
+                        GetAllInfoProjects(AllProjects);
+                        break;
+                    case 3:
+                        GetAllInfoEmployees(AllEmployees);
+                        break;
+                    case 4:
+                        Console.Write("Enter project id: ");
+                        projId = Convert.ToInt32(Console.ReadLine());
+                        GetProjectWithId(projId, AllProjects);
+                        break;
+                    case 5:
+                        Console.Write("Enter employee id: ");
+                        empId = Convert.ToInt32(Console.ReadLine());
+                        GetEmployeeWithId(empId, AllEmployees);
+                        break;
+                    case 6:
+                        Console.Write("Enter project id: ");
+                        projId = Convert.ToInt32(Console.ReadLine());
+                        GetProjectEmployees(projId, AllProjects);
+                        break;
+                    case 7:
+                        Console.Write("Enter employee id: ");
+                        empId = Convert.ToInt32(Console.ReadLine());
+                        GetEmployeeProjects(empId, AllEmployees);
+                        break;
+                    case 8:
+                        Console.Write("Which project you want to assign an employee: ");
+                        projId = Convert.ToInt32(Console.ReadLine());
+                        prjIndex = FindProject(projId, AllProjects);
+                        if (!(prjIndex >= 0))
+                        {
+                            Console.WriteLine($"The project with id {projId} does not exist!");
+                            break;
+                        }
+
+                        Console.Write($"Which employee you want to assign project {AllProjects[prjIndex].Name}: ");
+                        empId = Convert.ToInt32(Console.ReadLine());
+                        empIndex = FindEmployee(empId, AllEmployees);
+                        if (!(empIndex >= 0))
+                        {
+                            Console.WriteLine($"The employee with id {empId} does not exist!");
+                            break;
+                        }
+
+                        AssignEmployeeToProject(AllProjects[prjIndex], AllEmployees[empIndex]);
+                        Console.WriteLine($"Employee {AllEmployees[empIndex].FullName} successfully assigned to project {AllProjects[prjIndex].Name}");
+                        break;
+                    case 9:
+                        Console.Write("Which project you want to remove an employee: ");
+                        projId = Convert.ToInt32(Console.ReadLine());
+                        prjIndex = FindProject(projId, AllProjects);
+                        if (!(prjIndex >= 0))
+                        {
+                            Console.WriteLine($"The project with id {projId} does not exist!");
+                            break;
+                        }
+
+                        Console.Write($"Which employee you want to remove from project {AllProjects[prjIndex].Name}: ");
+                        empId = Convert.ToInt32(Console.ReadLine());
+                        empIndex = FindEmployee(empId, AllEmployees);
+                        if (!(empIndex >= 0))
+                        {
+                            Console.WriteLine($"The employee with id {empId} does not exist!");
+                            break;
+                        }
+
+                        RemoveEmployeeFromProject(AllProjects[prjIndex], empId);
+                        Console.WriteLine($"Employee {AllEmployees[empIndex].FullName} successfully removed from project {AllProjects[prjIndex].Name}");
+                        break;
+                    case 10:
+                        Console.Write("Which project you want to leave: ");
+                        projId = Convert.ToInt32(Console.ReadLine());
+                        prjIndex = FindProject(projId, AllProjects);
+                        if (!(prjIndex >= 0))
+                        {
+                            Console.WriteLine($"No project on id {projId}!");
+                            break;
+                        }
+
+                        Console.Write($"Enter your employee id for leave project {AllProjects[prjIndex].Name}: ");
+                        empId = Convert.ToInt32(Console.ReadLine());
+                        empIndex = FindEmployee(empId, AllEmployees);
+                        if (!(empIndex >= 0))
+                        {
+                            Console.WriteLine($"No employee on id {empId}!");
+                            break;
+                        }
+                        AllEmployees[empIndex].Leave(projId);
+                        Console.WriteLine($"{AllEmployees[empIndex].FullName} leaves the project {AllProjects[prjIndex].Name}!");
+                        break;
+                    case 11:
+                        Console.Write("Which project ready to finish: ");
+                        projId = Convert.ToInt32(Console.ReadLine());
+                        prjIndex = FindProject(projId, AllProjects);
+                        if (!(prjIndex >= 0))
+                        {
+                            Console.WriteLine($"The project with id {projId} does not exist!");
+                            break;
+                        }
+                        AllProjects[prjIndex].Finish();
+                        break;
+                    case 12:
+                        isPlaying = false;
+                        break;
+                    default:
+                        Console.WriteLine("0 - Create project\n1 - Create employee\n2 - All projects\n3 - All employees");
+                        Console.WriteLine("4 - Project detailed info\n5 - Employee detailed info");
+                        Console.WriteLine("6 - Project employees info\n7 - Employee project info");
+                        Console.WriteLine("8 - Assign employee to project\n9 - Remove employee from project");
+                        Console.WriteLine("10 - Leave project\n11 - Finish the project\n12 - Exit the dashboard\n");
+                        break;
+                }
+            }
+        }
+
+        static void CreateProject(ref Project[] AllGivenProjects,ref Project[] CopyOfAllGivenProjects, int projId, string projName)
+        {
+            Array.Resize(ref CopyOfAllGivenProjects, CopyOfAllGivenProjects.Length + 1);
+            CopyOfAllGivenProjects[CopyOfAllGivenProjects.Length - 1] = new Project(projId, projName);
+            AllGivenProjects = CopyOfAllGivenProjects;
+        }
+
+        static void CreateEmployee(ref Employee[] AllGivenEmployees, ref Employee[] CopyOfAllGivenEmployees, int empId, string empFullName)
+        {
+            Array.Resize(ref CopyOfAllGivenEmployees, CopyOfAllGivenEmployees.Length + 1);
+            CopyOfAllGivenEmployees[CopyOfAllGivenEmployees.Length - 1] = new Employee(empId, empFullName);
+            AllGivenEmployees = CopyOfAllGivenEmployees;
+        }
+
+        static void GetAllInfoProjects(Project[] AllGivenProjects)
+        {
+            Console.WriteLine($"Number of projects: {AllGivenProjects.Length}");
+            int count = 0;
+            foreach (var prj in AllGivenProjects)
+            {
+                count++;
+                Console.WriteLine($"{count}: {prj.Id} {prj.Name}");
+            }
+        }
+
+        static void GetAllInfoEmployees(Employee[] AllGivenEmployees)
+        {
+            Console.WriteLine($"Number of employees: {AllGivenEmployees.Length}");
+            int count = 0;
+            foreach (var emp in AllGivenEmployees)
+            {
+                count++;
+                Console.WriteLine($"{count}: {emp.Id} {emp.FullName}");
+            }
+        }
+
+        static void GetProjectWithId(int id, Project[]? AllGivenProjects)
+        {
+            if (AllGivenProjects == null) return;
+
+            int index = FindProject(id, AllGivenProjects);
+
+            if (index < 0)
+            {
+                Console.WriteLine($"The project with id {id} does not exist!");
+                return;
+            }
+            else
+            { 
+                AllGivenProjects[index].GetInfo();
+            }
+        }
+
+        static void GetEmployeeWithId(int id, Employee[]? AllGivenEmployees)
+        {
+            if (AllGivenEmployees == null) return;
+
+            int index = FindEmployee(id, AllGivenEmployees);
+
+            if (index < 0)
+            {
+                Console.WriteLine($"The employee with id {id} does not exist!");
+                return;
+            }
+            else
+            {
+                AllGivenEmployees[index].GetInfo();
+            }
+        }
+
+        static void GetProjectEmployees(int id, Project[]? AllGivenProjects)
+        {
+            if (AllGivenProjects == null) return;
+
+            int index = FindProject(id, AllGivenProjects);
+
+            if (index < 0)
+            {
+                Console.WriteLine($"The project with id {id} does not exist!");
+                return;
+            }
+            else
+            { 
+                AllGivenProjects[index].GetInfoEmployees();
+            }
+
+        }
+
+        static void GetEmployeeProjects(int id, Employee[]? AllGivenEmployees)
+        {
+            if (AllGivenEmployees == null) return;
+
+            int index = FindEmployee(id, AllGivenEmployees);
+
+            if (index < 0)
+            {
+                Console.WriteLine($"The employee with id {id} does not exist!");
+                return;
+            }
+            else
+            {
+                AllGivenEmployees[index].GetInfoProjects();
+            }
+        }
+
+        static void AssignEmployeeToProject(Project project, Employee employee)
+        {
+            project.AssignEmployee(employee);
+        }
+
+        static void RemoveEmployeeFromProject(Project project, int empId)
+        {
+            project.RemoveEmployee(empId);
+        }
+
+        static int FindProject(int id, Project[]? AllGivenProjects) // helper function for find project index in Projects array
+        {
+            if (AllGivenProjects == null) return -1;
+
+            int index = 0;
+            int count = 0;
+            for (int i = 0; i < AllGivenProjects.Length; i++)
+            {
+                if (AllGivenProjects[i].Id == id)
+                {
+                    index = i;
+                    break;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            if (count == AllGivenProjects.Length)
+            {
+                return -1;
+            }
+
+            return index;
+        }
+
+        static int FindEmployee(int id, Employee[]? AllGivenEmployees) // helper function for find employee index in Employees array
+        {
+            if (AllGivenEmployees == null) return -1;
+
+            int index = 0;
+            int count = 0;
+            for (int i = 0; i < AllGivenEmployees.Length; i++)
+            {
+                if (AllGivenEmployees[i].Id == id)
+                {
+                    index = i;
+                    break;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            if (count == AllGivenEmployees.Length)
+            {
+                return -1;
+            }
+
+            return index;
+        }
+    }
+}
