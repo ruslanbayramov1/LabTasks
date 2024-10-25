9c9e9c5b65e89cb6caa576e8893ff66e8c3bbdad
ruslanbayramov1 <ruslanyb-ab108@code.edu.az>
2024-10-25 18:30:33 +0400

    operator overloading car task

---
+++ 15_Operator_Overloading/Car.cs
@@ -0,0 +1,55 @@
+﻿namespace _15_Operator_Overloading;
+
+internal class Car
+{
+    public int Id { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public int EngineCode { get; set; }
+
+    public Car(int id, string brand, string model, int year, int engineCode)
+    {
+        Id = id;
+        Brand = brand;
+        Model = model;
+        Year = year;
+        EngineCode = engineCode;
+    }
+
+    public static bool operator <(Car leftCar, Car rightCar)
+    {
+        return leftCar.Year < rightCar.Year;
+    }
+
+    public static bool operator >(Car leftCar, Car rightCar)
+    {
+        return leftCar.Year > rightCar.Year;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.GetType().ToString().Split(".")[^1]} {{ Id: {Id}, Brand: {Brand}, Model: {Model}, Year: {Year}, EngineCode: {EngineCode} }}";
+    }
+}
+
+static class Extension
+{
+    public static Car[] OrderByYear(this Car[] cars)
+    {
+        Car[] newCars = (Car[])cars.Clone();
+        for (int i = 0; i < newCars.Length; i++)
+        {
+            for (int j = 0; j < newCars.Length - 1; j++)
+            {
+                if (newCars[j] > newCars[j + 1])
+                {
+                    Car temp = newCars[j];
+                    newCars[j] = newCars[j + 1];
+                    newCars[j + 1] = temp;
+                }
+            }
+        }
+        return newCars;
+    }
+}
---
+++ 15_Operator_Overloading/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _15_Operator_Overloading;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Car[] cars =
+        {
+                new Car(1, "Mercedes", "AMG GT", 2022, 13492),
+                new Car(2, "BMW", "M5", 2018, 99425),
+                new Car(3, "AUDI", "R8", 2020, 23482)
+            };
+
+        Car[] orderedCars = cars.OrderByYear();
+
+        foreach (Car car in orderedCars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
